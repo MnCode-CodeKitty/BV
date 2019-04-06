b20247325cebb24d9ef307b48db6d1f707aa5e30
codekitty-jason <jason@codekitty.org>
2019-04-06 12:50:24 -0500

    trying to fix sheet update try 1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
 // The publicSpreadSheetUrl is the Students tab of the sheet.  Copy from the address bar including gid!
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1NkVsOEoXGicyensK9Fn014-vCdeXk0q8ilXwA4HTISY/edit#gid=1827105956';
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1NkVsOEoXGicyensK9Fn014-vCdeXk0q8ilXwA4HTISY#gid=1827105956';
 
 // The publicCourseSheetUrl is the Courses tab of the sheet.  Copy from the address bar including gid!
-var publicCourseSheetUrl = 'https://docs.google.com/spreadsheets/d/1NkVsOEoXGicyensK9Fn014-vCdeXk0q8ilXwA4HTISY/edit#gid=1555010656';
-
-
-
-
-
+var publicCourseSheetUrl = 'https://docs.google.com/spreadsheets/d/1NkVsOEoXGicyensK9Fn014-vCdeXk0q8ilXwA4HTISY#gid=1555010656';
 
 document.getElementById("studentBox").innerHTML = "<center><h3>Your info will go here!</h3></center>";
 
